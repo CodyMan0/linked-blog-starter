0317506df428024eea3bcac29403df443b45d24c
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-03-23 17:47:25 +0900

    feat : About 및 Project 페이지 라우터 설정

---
+++ pages/about/index.tsx
@@ -0,0 +1,5 @@
+import Layout from "../../components/misc/layout";
+
+export default function About() {
+	return <Layout>about</Layout>;
+}
---
+++ pages/project/index.tsx
@@ -0,0 +1,5 @@
+import Layout from "../../components/misc/layout";
+
+export default function Project() {
+	return <Layout>project</Layout>;
+}
