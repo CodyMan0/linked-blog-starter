bbd829bdf10d948fd839fb6fa212ef531e06dcf7
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-03-24 00:21:23 +0900

    feat: nprogress 라이브러리를 활용하여 loading 기능 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,13 @@ import "../styles/index.css";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
 import Layout from "../components/misc/layout";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
