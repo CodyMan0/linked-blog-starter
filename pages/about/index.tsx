57ebf7714353414d1907aab215ecc3244fd07fcf
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-04-06 22:17:19 +0900

    design: 소개페이지 글 수정

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,12 +4,12 @@ export default function About() {
 			<div className="absolute top-[10%] m-10 ">
 				<h1 className="text-4xl">Hi~ Brian here!</h1>
 				<h2>
-					모든 사람들이 일상 가운데 사용해야만 하는 서비스를 개발하는게 제
-					목표입니다.
+					안녕하세요 🥰 찾아와 주셔서 감사합니다. 전 기술을 활용하여 모든
+					사람들의 일상 가운데 녹아드는 서비스를 개발하고자 합니다. 가치를
+					만드는데 집중합니다.
 				</h2>
 				<br />
-				<div>안녕하세요 🥰 찾아와 주셔서 감사합니다.</div>
-				<button>button</button>
+				<div></div>
 			</div>
 		</div>
 	);
