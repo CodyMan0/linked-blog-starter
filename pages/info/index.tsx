cd166772abdb18670343055040aedd8addc029e0
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-05-23 20:43:32 +0900

    Design : 사이트 정보 UI 수정

--- pages/info/index.tsx
+++ pages/info/index.tsx
@@ -1,17 +1,34 @@
+import { NextSeo } from "next-seo";
+import Link from "next/link";
+
 export default function Info() {
 	return (
-		<div>
-			<h1>Site Information</h1>
-			<h2>Resources 📚</h2>
-			This site was made possible with...
-			<ul>
-				<li>
-					Gatsby Starter Template: LekoArts/gatsby-starter-portfolio-jodie
-				</li>
-				<li>Notion Plugin: orlowdev/gatsby-source-notion-api</li>
-				Blog Components: scttcper/gatsby-casper
-				<li></li>
-			</ul>
-		</div>
+		<>
+			<NextSeo
+				title="Info"
+				openGraph={{
+					description: "사이드 정보 입니다.",
+					type: "website",
+				}}
+			/>
+			<div className="w-full p-2 lg:p-16 max-w-screen-xl mx-auto my-0">
+				<h1 className="text-4xl font-extrabold my-7">사이드 정보</h1>
+				<h2 className="text-2xl font-bold my-7">리소스 📚</h2>
+				<ul>
+					<li className="tracking-wider mt-3">
+						해당 웹 페이지는 Next.js로 만들어졌습니다.
+					</li>
+					<li className="tracking-wider mt-3">
+						옵시디언과 연동시켜주는 오픈 소스 :
+						<Link
+							href="https://github.com/matthewwong525/linked-blog-starter"
+							className="underline underline-offset-4 text-link ml-4"
+						>
+							matthewwong525 / linked-blog-starter
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</>
 	);
 }
